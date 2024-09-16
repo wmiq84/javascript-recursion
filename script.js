25ae543db6386b05baff9fe383d3002403832585
wmiq84 <michaelwang3489@gmail.com>
2024-09-15 20:04:07 -0700

    Fix bug in merge sort

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 function generateFibonacci(num) {
-    
 	if (num === 1) return 0;
 	else if (num === 2) return 1;
 
-    let array = [];
-	var generateFibonacci(num - 1) + generateFibonacci(num - 2);
-    array.push(var)
-	// array.push(currentNum);
-    // return array;
+	return generateFibonacci(num - 1) + generateFibonacci(num - 2);
 }
 
-// console.log(generateFibonacci(12));
+function mergeSort(array) {
+	let emptyArray = [];
+	let leftArray = [];
+	let rightArray = [];
 
-// function mergeSort(array) {
-//     array.sort()
-//     array.split();
-// }
+	if (leftArray.length === 1 && rightArray.length === 1) {
+		emptyArray.push(newArray);
+		emptyArray.sort((a, b) => {
+			return a - b;
+		});
+	}
+
+	leftArray = array.slice(0, array.length / 2);
+	rightArray = array.slice(array.length / 2);
+
+	console.log(leftArray);
+	console.log(rightArray);
+
+	return
+	// return (newArray = array.slice(newArray.length / 2));
+}
+
+console.log(mergeSort([0, 2, 3, 4, 6, 4, 7, 9, 1]));
