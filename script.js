305ea924d8ac3447d85f6d7500c739a540645488
wmiq84 <michaelwang3489@gmail.com>
2024-09-15 18:09:21 -0700

    Initialize files

---
+++ script.js
@@ -0,0 +1,18 @@
+function generateFibonacci(num) {
+    
+	if (num === 1) return 0;
+	else if (num === 2) return 1;
+
+    let array = [];
+	var generateFibonacci(num - 1) + generateFibonacci(num - 2);
+    array.push(var)
+	// array.push(currentNum);
+    // return array;
+}
+
+// console.log(generateFibonacci(12));
+
+// function mergeSort(array) {
+//     array.sort()
+//     array.split();
+// }
